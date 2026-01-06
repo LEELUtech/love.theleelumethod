2055579ee2dd03c080710e3dc7224fc3a58ae57d
Mykola Tyshchenko <nikolay.tyshchenko04@gmail.com>
2026-01-06 18:56:36 +0200

    feat: implement PillButton component and refactor button usage across the application

--- src/components/ui/buttons/FreeQuizButton.tsx
+++ src/components/ui/buttons/FreeQuizButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import PillButton from "@/components/ui/buttons/PillButton";
 import { QUIZ_URL } from "@/utils/constants"
 import React from "react";
 
@@ -9,12 +10,11 @@ interface FreeQuizButtonProps {
 
 export default function FreeQuizButton({ text = "Free Quiz" }: FreeQuizButtonProps) {
 	return (
-		<a
+		<PillButton
 			href={QUIZ_URL}
 			rel="noopener noreferrer"
-			className="bg-pink-600 hover:bg-pink-700 text-white font-bold py-3 px-12 rounded-full text-lg shadow-lg transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-pink-400/50 text-center inline-block"
 		>
-			{text}
-		</a>
+			<span>{text}</span>
+		</PillButton>
 	);
 }
