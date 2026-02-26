7c44e099913aa38f95e7f7fe228834e59308095a
ab3MN <ab3mn3@gmail.com>
2026-02-26 13:24:33 +0200

    fix:WebinarHeyImLilySection

--- src/app/decode/page.tsx
+++ src/app/decode/page.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import WebinarSection from "@/components/sections/Webinar/WebinarSection";
-import WebinarContentSection from "@/components/sections/Webinar/WebinarContentSection";
-import Footer from "@/components/ui/Footer";
-import WebinarDiscoverSection from "@/components/sections/Webinar/WebinarDiscoverSection";
-import WebinarWhatIfSection from "@/components/sections/Webinar/WebinarWhatIfSection";
-import WebinarHeyImLilySection from "@/components/sections/Webinar/WebinarHeyImLilySection";
-import TestimonialsSection from "@/components/sections/Webinar/TestimonialsSection"
+import React from 'react';
+import WebinarSection from '@/components/sections/Webinar/WebinarSection';
+import WebinarContentSection from '@/components/sections/Webinar/WebinarContentSection';
+import WebinarDiscoverSection from '@/components/sections/Webinar/WebinarDiscoverSection';
+import WebinarWhatIfSection from '@/components/sections/Webinar/WebinarWhatIfSection';
+import WebinarHeyImLilySection from '@/components/sections/Webinar/WebinarHeyImLilySection';
+import TestimonialsSection from '@/components/sections/Webinar/TestimonialsSection';
+import FooterLayout from '@/components/ui/footer/FooterLayout';
+import HeyImLilySection from '@/components/sections/Secrets/HeyImLilySection';
 
 const WebinarPage = () => {
-	return (
-		<main>
-			<WebinarSection />
-			<TestimonialsSection/>
-			<WebinarContentSection />
-			<WebinarWhatIfSection />
-			<WebinarDiscoverSection />
-			<WebinarHeyImLilySection />
-			<Footer />
-		</main>
-	);
+  return (
+    <main>
+      <WebinarSection />
+      <TestimonialsSection />
+      <WebinarContentSection />
+      <WebinarWhatIfSection />
+      <WebinarDiscoverSection />
+
+      <HeyImLilySection />
+      <WebinarHeyImLilySection />
+
+      <FooterLayout withNavigation={false} />
+    </main>
+  );
 };
 
 export default WebinarPage;
--- src/components/ui/footer/FooterLayout.tsx
+++ src/components/ui/footer/FooterLayout.tsx
@@ -7,10 +7,10 @@ interface Props {
 }
 
 const FooterLayout = (props: Props) => {
-  const { className, withNavigation = true } = props;
+  const { className = '', withNavigation = true } = props;
 
   return (
-    <footer className={`bg-white py-[22px]  ${className}`}>
+    <footer className={`bg-white pb-[72px] pt-6 lg:pb-[180px]  ${className}`}>
       <div className='max-w-[1600px] mx-auto px-4 md:px-6 lg:px-8 2xl:px-[180px]'>
         <FooterLabel mb={10} />
         {withNavigation && <FooterNavigation />}
