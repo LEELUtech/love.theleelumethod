7f499a5880b073deb2ed1fa6c82c371aeccf8583
ab3MN <ab3mn3@gmail.com>
2026-02-27 16:28:20 +0200

    feat:add static links

---
+++ src/static/index.tsx
@@ -0,0 +1,280 @@
+export interface ILink {
+  label: string;
+  href: string;
+}
+
+type RecordLink = Record<string, ILink>;
+
+export const RESOURCES_LINKS: RecordLink = {
+  CARD_1_LINK: {
+    label: 'DOWNLOAD FREE GUIDE',
+    href: '#',
+  },
+  CARD_2_LINK: {
+    label: 'GET THE REPORT',
+    href: '#',
+  },
+  CARD_3_LINK: {
+    label: 'START QUIZ',
+    href: '#',
+  },
+  DECODED_LINK: {
+    label: 'Save My Seat',
+    href: '#',
+  },
+};
+
+export const SECRETS_LINKS: RecordLink = {
+  DOWNLOAD_LINK: {
+    label: 'DOWNLOAD THE FREE GUIDE',
+    href: '#',
+  },
+  ACCESS_LINK: {
+    label: 'ACCESS THE FREE GUIDE',
+    href: '#',
+  },
+  CLAIM_LINK: {
+    label: 'CLAIM YOUR FREE GUIDE',
+    href: '#',
+  },
+};
+
+export const DESCODE_LINKS: RecordLink = {
+  SAVE_HERO_LINK: {
+    label: 'Save My Seat',
+    href: '#',
+  },
+  RESERVE_LINK: {
+    label: 'Reserve My Spot',
+    href: '#',
+  },
+  REGISTER_LINK: {
+    label: 'REGISTER NOW',
+    href: '#',
+  },
+  FOOTER_PROMO_LINK: {
+    label: 'Save My Seat',
+    href: '#',
+  },
+};
+
+export const COMPABILITY_REPORT_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'GIVE ME MY COMPATIBILITY CODE REPORT',
+    href: '#',
+  },
+  SECOND_LINK: {
+    label: 'GIVE ME MY COMPATIBILITY CODE REPORT',
+    href: '#',
+  },
+  UNLOCK_LINK: {
+    label: 'UNLOCK MY COMPATIBILITY CODE - $3.33',
+    href: '#',
+  },
+  FOOTER_PROMO_LINK: {
+    label: 'GIVE ME MY COMPATIBILITY CODE REPORT',
+    href: '#',
+  },
+};
+
+export const ABOUT_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'START THE DECODE',
+    href: '#',
+  },
+  FRACTURE_LINK: {
+    label: 'START THE DECODE',
+    href: '#',
+  },
+  SYNTHESIS_LINK: {
+    label: 'START THE DECODE',
+    href: '#',
+  },
+  FOOTER_PROMO_LINK: {
+    label: 'Access your Operating Manual',
+    href: '#',
+  },
+};
+
+export const PROGRAMS_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'ENROLL NOW',
+    href: '#',
+  },
+  FRACTURE_LINK: {
+    label: 'READ MORE STORIES',
+    href: '#stories', // Stories
+  },
+  PROTOCOL_LINK: {
+    label: 'INITIATE THE PROTOCOL',
+    href: '#',
+  },
+  SELF_GUIDED_LINK: {
+    label: 'BEGIN THE PROTOCOL',
+    href: '/programs/self-guided-transformation',
+  },
+  BREAKTHROUGH_LINK: {
+    label: 'GET PERSONALIZED SUPPORT',
+    href: '/programs/guided-breakthrough',
+  },
+  VIP_LINK: {
+    label: 'APPLY FOR VIP ACCESS',
+    href: '/programs/vip-immersion',
+  },
+  PROTOCOL_ESSENTIALS_LINK: {
+    label: 'BEGIN PROTOCOL',
+    href: '/programs/self-guided-transformation#checkout',
+  },
+  PROTOCOL_BREAKTHROUGH_LINK: {
+    label: 'BREAK THROUGH',
+    href: '/programs/guided-breakthrough#checkout',
+  },
+  PROTOCOL_VIP_LINK: {
+    label: 'APPLY FOR VIP',
+    href: '/programs/vip-immersion#checkout',
+  },
+  ACCESS_LINK: {
+    label: 'ACCESS THE PROTOCOL',
+    href: '#',
+  },
+};
+
+export const HOME_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'START THE DECODE',
+    href: '#',
+  },
+  READ_LINK: {
+    label: 'READ MORE STORIES',
+    href: '#stories',
+  },
+  QUIZL_LINK: {
+    label: 'TAKE THE QUIZ',
+    href: '#',
+  },
+  REGISTER_LINK: {
+    label: 'REGISTER FOR MASTERCLASS',
+    href: '#',
+  },
+  PERSONALIZED_LINK: {
+    label: 'GET MY PERSONALIZED REPORT',
+    href: '#',
+  },
+  DECODED_LINK: {
+    label: 'Save My Seat',
+    href: '#',
+  },
+  DECODED_LOVE_LINK: {
+    label: 'Save My Seat',
+    href: '#',
+  },
+  RELATIONSHIP_LINK: {
+    label: 'VIEW THE CURRICULUM',
+    href: '#',
+  },
+  PROTOCOL_BREAKTHROUGH_LINK: {
+    label: 'BREAK THROUGH',
+    href: '/programs/guided-breakthrough#checkout',
+  },
+  FOOTER_PROMO_LINK: {
+    label: `YES, I'M IN`,
+    href: '#',
+  },
+};
+
+export const ESSENTIALS_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'ENROLL NOW',
+    href: '#',
+  },
+  INSIDE_LINK: {
+    label: 'SECURE YOUR SPOT',
+    href: '#',
+  },
+  RESULT_LINK: {
+    label: 'BEGIN THE PROTOCOL',
+    href: '#',
+  },
+  WORKED_LINK: {
+    label: 'BEGIN THE PROTOCOL',
+    href: '#',
+  },
+  COST_LINK: {
+    label: 'DECODE MY RELATIONSHIP',
+    href: '#',
+  },
+  CHECKOUT_LINK: {
+    label: 'DECODE MY RELATIONSHIP',
+    href: '#',
+  },
+};
+
+export const GUIDED_BREAKTHROUGH_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'GET PERSONAL',
+    href: '#',
+  },
+  COMPATIBILITY_LINK: {
+    label: 'ENROLL NOW',
+    href: '#',
+  },
+  WORKED_LINK: {
+    label: 'GET PERSONALIZED SUPPORT',
+    href: '#',
+  },
+  COST_LINK: {
+    label: 'CLAIM MY BREAKTHROUGH',
+    href: '#',
+  },
+  CHECKOUT_LINK: {
+    label: 'SIGN UP & GET PERSONALIZED SUPPORT',
+    href: '#',
+  },
+};
+
+export const VIP_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'APPLY FOR ACCESS',
+    href: '#',
+  },
+  MECHANICS_LINK: {
+    label: 'SECURE YOUR SPOT',
+    href: '#',
+  },
+  RESULT_LINK: {
+    label: 'APPLY FOR VIP ACCESS',
+    href: '#',
+  },
+  WHO_LINK: {
+    label: 'APPLY FOR VIP ACCESS',
+    href: '#',
+  },
+  CHECKOUT_LINK: {
+    label: 'SIGN UP & APPLY FOR VIP ACCESS',
+    href: '#',
+  },
+};
+
+export const ONE_ON_ONE_LINKS: RecordLink = {
+  HERO_LINK: {
+    label: 'BOOK YOUR SESSION',
+    href: '#',
+  },
+  SINGLE_SESSION_LINK: {
+    label: 'BOOK SESSION',
+    href: '#',
+  },
+  THREE_PACKAGE_LINK: {
+    label: 'BOOK PACKAGE',
+    href: '#',
+  },
+  NINE_PACKAGE_LINK: {
+    label: 'BOOK PACKAGE',
+    href: '#',
+  },
+
+  PRIMARY_LINK: {
+    label: 'APPLY FOR VIP ACCESS',
+    href: '#',
+  },
+};
