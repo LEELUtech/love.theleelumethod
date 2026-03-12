792800a2fdb1531f314e62168728e9c571a6c074
ab3MN <ab3mn3@gmail.com>
2026-03-12 12:34:27 +0200

    refactor:hadnleCirclePayment

--- functions/src/utils/helpers/circle/index.ts
+++ functions/src/utils/helpers/circle/index.ts
@@ -2,3 +2,4 @@ export { getUserData } from "./getUserData";
 export { getOffering } from "./getOffering";
 export { sendWelcomeMessage } from "./sendWelcomeMessage";
 export { getTier } from "./getTier";
+export { handleCircleProduct } from "./handleCircleProduct";
